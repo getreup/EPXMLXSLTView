bedd4860fea238950ebb5b1e76bb11a7b582f889
KelseyRegan <kelseyregan@Patricks-MacBook-Pro-2.local>
2013-10-22 19:08:53 -0700

    Fixing name

--- EPXSLTXMLView/EPXMLXSLTView/main.m
+++ EPXSLTXMLView/EPXMLXSLTView/main.m
@@ -1,6 +1,6 @@
 //
 //  main.m
-//  EPXSLTXMLView
+//  EPXMLXSLTView
 //
 //  Created by Kelsey Regan on 2013-10-22.
 //  Copyright (c) 2013 Elevated Pixels Software. All rights reserved.
---
+++ EPXSLTXMLView/EPXMLXSLTViewTests/EPXMLXSLTViewTests.m
@@ -0,0 +1,34 @@
+//
+//  EPXMLXSLTViewTests.m
+//  EPXMLXSLTViewTests
+//
+//  Created by Kelsey Regan on 2013-10-22.
+//  Copyright (c) 2013 Elevated Pixels Software. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EPXMLXSLTViewTests : XCTestCase
+
+@end
+
+@implementation EPXMLXSLTViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
